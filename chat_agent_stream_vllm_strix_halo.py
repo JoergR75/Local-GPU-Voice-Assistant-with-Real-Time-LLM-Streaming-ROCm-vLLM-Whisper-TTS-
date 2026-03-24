5cb380b3d6b6005eacbb2992517a97d963a72ffb
Jörg Roskowetz <141847198+JoergR75@users.noreply.github.com>
2026-03-24 11:19:25 +0100

    Update chat_agent_stream_vllm_strix_halo.py

--- chat_agent_stream_vllm_strix_halo.py
+++ chat_agent_stream_vllm_strix_halo.py
@@ -222,7 +222,7 @@ if __name__ == "__main__":
     ## 🚀 Hardware & Platform
     Running fully local on:
     **AMD Ryzen™ AI MAX 390 w/ Radeon 8050S**
-    Powered by **ROCm 7.0.0**
+    Powered by **ROCm 7**
     Ubuntu 24.04
 
     ## 🎤 How to Use
@@ -239,7 +239,7 @@ if __name__ == "__main__":
     [![PyTorch](https://img.shields.io/badge/PyTorch-2.11.0%20(Preview)-ee4c2c?logo=pytorch)](https://pytorch.org/get-started/locally/)
     [![Docker](https://img.shields.io/badge/Docker-29.2.0-blue?logo=docker)](https://www.docker.com/)
     [![Ubuntu](https://img.shields.io/badge/Ubuntu-22.04%20%7C%2024.04-e95420?logo=ubuntu)](https://ubuntu.com/download/server)
-    [![AMD Radeon AI PRO R9700](https://img.shields.io/badge/AMD-RDNA4%20Radeon(TM)%20AI%20PRO%20R9700-8B0000?logo=amd)](https://www.amd.com/en/products/graphics/workstations/radeon-ai-pro/ai-9000-series/amd-radeon-ai-pro-r9700.html)
+    [![AMD Radeon AI MAX 390](https://img.shields.io/badge/AMD-RDNA4%20Radeon(TM)%20AI%20PRO%20R9700-8B0000?logo=amd)](https://www.amd.com/en/products/processors/laptop/ryzen/ai-300-series/amd-ryzen-ai-max-390.html)
 
     ### 😏 Warning
     Responses may contain sarcasm, wit, and dangerously high GPU utilization.
