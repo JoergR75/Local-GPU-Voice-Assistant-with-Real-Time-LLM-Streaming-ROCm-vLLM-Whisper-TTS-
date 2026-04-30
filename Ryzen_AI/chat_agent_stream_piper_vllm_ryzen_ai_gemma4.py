57b2c5fb7a7df8d8c854a21c99a671bbef2b78a7
Jörg Roskowetz <141847198+JoergR75@users.noreply.github.com>
2026-04-30 18:30:55 +0200

    Update chat_agent_stream_piper_vllm_ryzen_ai_gemma4.py

--- Ryzen_AI/chat_agent_stream_piper_vllm_ryzen_ai_gemma4.py
+++ Ryzen_AI/chat_agent_stream_piper_vllm_ryzen_ai_gemma4.py
@@ -68,12 +68,12 @@ from vllm import LLM, SamplingParams
 # -----------------------------
 # MODEL_ID = "/app/models/llama3-3-8b-heretic"
 # MODEL_ID = "/app/models/qwen3-4b-instruct-2507"
-MODEL_ID = "/app/models/gemma-4-e4b-uncensored"
+MODEL_ID = "Phu-Hien/gemma_4_dkkd_lora_vllm"
 # MODEL_ID = "chohtet/Qwen2.5-7B-Instruct-H3-VLLM"
 # MODEL_ID = "predibase/Mistral-7B-Instruct-v0.2-medusa-vllm "
 # MODEL_ID = "Orenguteng/Llama-3.1-8B-Lexi-Uncensored-V2"
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_ID, local_files_only=True)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
 
 # -----------------------------
 # Faster-Whisper (Speech → Text)
